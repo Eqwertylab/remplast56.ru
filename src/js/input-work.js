ab277511ed5cdf1a4746f87bbb7268d6a12e0035
Dostoevsky <79225555735@ya.ru>
2016-03-23 23:13:24 +0500

    fix: pancall style, search js error, service list

--- src/js/input-work.js
+++ src/js/input-work.js
@@ -2,10 +2,10 @@ var forms = document.forms;
 for(var i = 0; i < forms.length; i++) {
 	forms[i].addEventListener('focus', function(e) {
 		var label = this.querySelector('label[for="'+e.target.id+'"]');
-		label.classList.add('form__label_active');
+		label ? label.classList.add('form__label_active') : '';
 	}, true);
 	forms[i].addEventListener('blur', function(e) {
 		var label = this.querySelector('label[for="'+e.target.id+'"]');
-		label.classList.remove('form__label_active');
+		label ? label.classList.remove('form__label_active') : '';
 	}, true);
 }
