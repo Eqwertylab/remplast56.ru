2e59f35d16a8d2c45a344d4cbd323632bc8ff792
Dostoevsky <79225555735@ya.ru>
2016-03-27 12:51:06 +0500

    add animate and fix modal bug

--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,27 @@
 $('html').click(function() {
-    $('#search').removeClass('search_active');
-});
-$('#search-toggle').click(function() {
-    $('#search').toggleClass('search_active');
-    $('#search-input').focus();
+	var search = $('#search');
+    search
+    	.addClass('search_animate-out')
+    	.removeClass('search_animate-in search_active');
 });
 $('#search').click(function(event){
     event.stopPropagation();
 });
+$('#search-toggle').click(function() {
+	var search = $('#search');
+	if(!$(this).hasClass('search_active')) {
+		search
+			.removeClass('search_animate-out')
+			.addClass('search_animate-in search_active');
+
+		$('#search-form').one("otransitionend oTransitionEnd msTransitionEnd transitionend",
+		function(e) {
+			setTimeout(function() {
+					$('#search-input').focus();
+				}, 500);
+		});
+	}
+});
+
+
+
