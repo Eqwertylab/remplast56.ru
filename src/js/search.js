bc5d378360745deba80c5717de7e95b96ac7a2f0
Dostoevsky <79225555735@ya.ru>
2016-03-27 14:52:22 +0500

    fix search no hide bug on ipad

--- src/js/search.js
+++ src/js/search.js
@@ -1,27 +1,32 @@
-$('html').click(function() {
-	var search = $('#search');
+function searchHide() {
+  var search = $('#search');
     search
-    	.addClass('search_animate-out')
-    	.removeClass('search_animate-in search_active');
-});
-$('#search').click(function(event){
+      .addClass('search_animate-out')
+      .removeClass('search_animate-in search_active');
+}
+
+$('body').on('click touchstart', searchHide);
+
+$('#search').on('click touchstart',function(event){
     event.stopPropagation();
 });
+
 $('#search-toggle').click(function() {
-	var search = $('#search');
-	if(!$(this).hasClass('search_active')) {
-		search
-			.removeClass('search_animate-out')
-			.addClass('search_animate-in search_active');
+  var search = $('#search');
+  if(!$(this).hasClass('search_active')) {
+    search
+      .removeClass('search_animate-out')
+      .addClass('search_animate-in search_active');
 
-		$('#search-form').one("otransitionend oTransitionEnd msTransitionEnd transitionend",
-		function(e) {
-			setTimeout(function() {
-					$('#search-input').focus();
-				}, 500);
-		});
-	}
+    $('#search-form').one("otransitionend oTransitionEnd msTransitionEnd transitionend",
+    function(e) {
+      if(isMobile) {
+        $('#search-input').focus();
+      } else {
+        setTimeout(function() {
+          $('#search-input').focus();
+        }, 500);
+      }
+    });
+  }
 });
-
-
-
